46708547b8f119ae9017fdbeee99d9cd8a9c44f3
hjw <hjw@naver.com>
2022-09-08 12:00:58 +0900

    2번째 수정

--- Project2/first.cpp
+++ Project2/first.cpp
@@ -2,8 +2,9 @@
 
 using namespace std;
 //깃허브 링크 올리기, 문제 풀어서
+//if문 안에서 변수 선언 가능
 
-class Teststruct
+/*class Teststruct
 {
 	int a;
 
@@ -30,5 +31,88 @@ void Teststruct::show()
 void main() {
 	Teststruct ob;
 	ob.show();
-} 
+} */
+
+/*if문 문제1, end 연산자 ?
+int main() {
+	
+	int numbers[3];
+	int max = 0;
+
+	cout << "정수 3개를 입력하시오:\n";
+
+	for (int i = 0; i < 3; i++) {
+		cin >> numbers[i];
+	}
+	for (int j = 0; j < 3; j++) {
+		if (max < numbers[j])
+			max = numbers[j];
+	}
+
+	cout << "가장 큰 정수는: " << max << "\n";
+
+	return 0;
+}*/
+
+/*switch문 문제1
+int main() {
+	char a;
+
+	cout << "What is your favorite food?\n";
+	cout << " a) kimch\n b) Korean BBQ\n c) Chicken\n";
+	
+	cin >> a;
+	switch (a)
+	{
+	case 'A':
+	case 'a':
+		cout << "kimch\n";
+		break;
+
+	case 'B':
+	case 'b':
+		cout << "Korean BBQ\n";
+		break;
+
+	case 'C':
+	case 'c':
+		cout << "Chicken\n";
+		break;
+
+	}
+
+	//case 'A':
+	    cout<< "covert to a \n" ;
+		[[failthrough]]; 여기에서 멈추지 않고 다른 실행가능 
+	  case 'a':
+		cout << "kimch\n";
+		break;
+	
+	return 0;
+}
+*/
+
+//피보나치 수열
+int main() {
+
+	int pre = 0; //이전 숫자
+	int cur = 1; //현재 숫자
+
+	int num;
+	cout << "숫자를 입력하시오:\n";
+	cin >> num;
+
+	if (num ==0)
+	else if(num==1)
+	
+	else 
+		for (int i = 0; i < num; i++) {
+
+		int next = pre + cur;
+		pre = cur;
+		cur = next;
+
+	}
+
+}
 
